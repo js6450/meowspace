43499476fa187136db849a52ffa5b61784a126c1
Jiwon Shin <js6450@nyu.edu>
2016-09-20 15:30:07 +0800

    another php attempt

--- public/upload.php
+++ public/upload.php
@@ -1,8 +1,14 @@
 <?php
+//
+//if(isset($_FILES["file"])){
+//    $dir = 'assets/';
+//    $blob = file_get_contents($_FILES["file"]["tmp_name"]);
+//    var_dump($blob);
+//    file_put_contents($dir.$_FILES["file"]["name"], $blob);
+//}
 
-if(isset($_FILES["file"])){
-    $dir = 'assets/';
-    $blob = file_get_contents($_FILES["file"]["tmp_name"]);
-    var_dump($blob);
-    file_put_contents($dir.$_FILES["file"]["name"], $blob);
-}
+$target_dir = "assets/";
+$target_file = $target_dir . basename($_FILES["file"]["name"]);
+$imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+
+echo $imageFileType;
